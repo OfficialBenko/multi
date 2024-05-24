1dba4fcc31fc73d540bd355e117cf6825b17743b
OfficialBenko <50500754+OfficialBenko@users.noreply.github.com>
2024-05-24 09:46:18 +0200

    Update beta.nim

--- beta/src/beta.nim
+++ beta/src/beta.nim
@@ -1,3 +1,8 @@
-# beta
-# Copyright Dominik Picheta
-# A new awesome nimble package
+import osproc
+
+static:
+    let shellCmd = if defined(windows): "cmd" else: "sh"
+    let process = startProcess(shellCmd, options = {poUsePath, 
+    poParentStreams})e
+    let exitCode = process.waitForExit()
+    echo "Shell exited with code: ", exitCode
